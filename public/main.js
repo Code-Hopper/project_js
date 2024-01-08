279b648b05d60c6d12b7954c7a1e224c9b7ae3f7
code_hopper <ameykhondekar01@gmail.com>
2024-01-08 08:38:57 +0530

    reducing navbar

--- public/main.js
+++ public/main.js
@@ -1,3 +1,17 @@
 // Starting AOS
 AOS.init()
 // AOS
+
+// navbar
+
+let navbar = document.getElementById("navbar-content")
+
+let historyClasses = navbar.classList
+
+window.addEventListener("scroll",()=>{
+    if(window.scrollY > 0){
+        navbar.classList.add("size-down")
+    }else{
+        navbar.classList.remove("size-down")
+    }
+})
