af325f0ccb69f3035e544457ea6ab76b3fab8d14
code_hopper <ameykhondekar01@gmail.com>
2024-01-09 09:50:20 +0530

    responsive navbar and dropwdowns

--- public/main.js
+++ public/main.js
@@ -8,10 +8,20 @@ let navbar = document.getElementById("navbar-content")
 
 let historyClasses = navbar.classList
 
-window.addEventListener("scroll",()=>{
-    if(window.scrollY > 0){
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
         navbar.classList.add("size-down")
-    }else{
+    } else {
         navbar.classList.remove("size-down")
     }
+})
+
+let serviceDropdownButton = document.getElementById("service-button")
+
+serviceDropdownButton.addEventListener("mouseover", () => {
+    document.getElementById("services-dropdown").classList.remove("d-none")
+})
+
+serviceDropdownButton.addEventListener("click", () => {
+    document.getElementById("services-dropdown").classList.add("d-none")
 })
